2392665b6b9579f04df05b540320f22b0318b447
Maksym <maksskazka1997@gmail.com>
2026-09-03 18:41:49 +0200

    fix: validate sessionId in auth middleware and clear session on expired refresh token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,6 +63,10 @@ export const refreshUserSession = async (req, res, next) => {
     }
 
     if (new Date() > new Date(session.refreshTokenValidUntil)) {
+      await Session.deleteOne({ _id: sessionId });
+      res.clearCookie('sessionId');
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
       throw createError(401, 'Session token expired');
     }
 
